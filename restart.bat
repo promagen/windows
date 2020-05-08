a805bb5a49d5cfad48beae9ca7a784ecc0ec6b30
tom <tom@sapletta.com>
2020-05-08 12:16:39 +0200

    update

--- restart.bat
+++ restart.bat
@@ -1,2 +1,3 @@
+@ECHO OFF
 ::cd src & node start-electron.js & cd ..
-cd php & stop.bat & start.bat & cd .. & cd src & stop.bat & start.bat & cd .. & browser.bat
+cd php & stop.bat & start.bat & cd .. & browser.bat & cd src & stop.bat & start.bat & cd ..
